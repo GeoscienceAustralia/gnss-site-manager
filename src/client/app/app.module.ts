35c68999736a2ea270c7641dbf8bc12466945591
Frank Fu <frank.fu@ga.gov.au>
2017-05-25 15:46:07 +1000

    Enable Prod mode to supress an error caused by empty values for required fields

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Injector, NgModule } from '@angular/core';
+import { Injector, NgModule, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
@@ -14,6 +14,21 @@ import { SiteLogModule } from './site-log/site-log.module';
 import { SelectSiteModule } from './select-site/select-site.module';
 import { UserRegistrationModule } from './user-registration/user-registration.module';
 import { AutoHeightDirective } from './shared/global/auto-height.directive';
+
+/*
+ * https://stackoverflow.com/questions/34868810/what-is-difference-between-production-and-development-mode-in-angular2
+ * https://angular.io/docs/ts/latest/api/core/index/enableProdMode-function.html
+ *
+ * Development mode - change detection does a second run immediately after the first run and produces an error if any
+ * bound value has changed between the first and the second run. This helps to locate bugs where checking values has
+ * side-effects or fields or functions don't return the same value on subsequent calls which undermines Angular's change detection.
+ *
+ * Production mode - disable Angular's development mode, which turns off assertions and other checks (2nd run of change
+ * detection as in dev mode) within the framework.
+ *
+ * Note: 'npm run build.prod' will enable production mode. So this is mainly for local environment.
+ */
+enableProdMode();
 
 @NgModule({
   imports: [
