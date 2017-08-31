acdc4b05c97905d12caef1e5415b33e79b98e0b4
Frank Fu <frank.fu@ga.gov.au>
2017-08-31 14:54:28 +1000

    Remove cors-setup service as it is not used

--- src/client/app/shared/cors-setup/index.ts
+++
@@ -1,4 +0,0 @@
-/**
- * This barrel file provides the export for the shared CorsSetupService.
- */
-export * from './cors-setup.service';
--- src/client/app/shared/index.ts
+++ src/client/app/shared/index.ts
@@ -1,7 +1,6 @@
 /**
  * This barrel file provides the exports for the shared resources (services, components).
  */
-export * from './cors-setup/index';
 export * from './http-request/index';
 export * from './global/index';
 export * from './service-worker/index';
