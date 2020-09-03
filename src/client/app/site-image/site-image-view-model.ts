868f585e107c9da8669ec82494230caf92a77721
Frank Fu <Frank.Fu@ga.gov.au>
2020-09-03 23:25:48 +1000

    remove time from dateTaken for site image component

--- src/client/app/site-image/site-image-view-model.ts
+++ src/client/app/site-image/site-image-view-model.ts
@@ -4,14 +4,4 @@ export class SiteImageViewModel {
     public fileReference: string;
     public imageType: string;
     public createdDate: string;
-
-    /*
-     * Format "YYYY-MM-DD HH:mm:ss" obtained from datetime picker to "YYYY-MM-DDTHH:mm:ss.SSSZ"
-     */
-    public formatCreatedDate(date: string): void {
-        this.createdDate = date.trim().replace(' ', 'T');
-        if (this.createdDate.length <= 19) {
-            this.createdDate += '.000Z';
-        }
-    }
 }
--- src/client/app/site-image/site-image.module.ts
+++ src/client/app/site-image/site-image.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { SelectModule } from 'ng2-select';
 import { FormInputModule } from '../shared/form-input/form-input.module';
 import { ThumbnailImageModule } from '../shared/thumbnail-image/thumbnail-image.module';
@@ -12,6 +13,7 @@ import { SiteImageComponent } from './site-image.component';
         FormInputModule,
         FormsModule,
         ReactiveFormsModule,
+        BsDatepickerModule.forRoot(),
         SelectModule,
         ThumbnailImageModule,
     ],
