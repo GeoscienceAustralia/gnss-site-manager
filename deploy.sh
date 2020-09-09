2c6f9d58b300c787d37ac1ace3c087e80df0a1c9
Frank Fu <Frank.Fu@ga.gov.au>
2020-09-09 15:44:43 +1000

    Remove unnecessary curly brackets

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,19 @@ fi
 
 env=$1
 
-if [ "${env}" == "prod" ]; then
+if [ "$env" == "prod" ]; then
     bucket="gnss-site-manager"
 else
-    bucket="${env}-gnss-site-manager"
+    bucket="$env-gnss-site-manager"
 fi
 
-backupBucket=${bucket}-backup
+backupBucket=$bucket-backup
 timestamp=$(date +'%Y%m%dT%H%M%S')
 
 if aws s3 --profile geodesy ls "s3://$backupBucket" 2>&1 | grep -q 'NoSuchBucket'; then
     aws s3 --profile geodesy mb "s3://$backupBucket"
 fi
 
-aws s3 --profile geodesy sync "s3://${bucket}" "s3://${backupBucket}/$timestamp"
-aws s3 --profile geodesy rm "s3://${bucket}" --recursive
-aws s3 --profile geodesy sync dist/prod "s3://${bucket}" --acl public-read
+aws s3 --profile geodesy sync "s3://$bucket" "s3://$backupBucket/$timestamp"
+aws s3 --profile geodesy rm "s3://$bucket" --recursive
+aws s3 --profile geodesy sync dist/prod "s3://$bucket" --acl public-read
